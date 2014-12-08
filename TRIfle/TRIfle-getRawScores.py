67ad0f471123eb4643746e87662cf5fba432632a
Robin van der Weide <robin@Robins-MacBook-Pro.local>
2014-12-08 13:49:14 +0000

    s

--- TRIfle/TRIfle-getRawScores.py
+++ TRIfle/TRIfle-getRawScores.py
@@ -161,7 +161,7 @@ coords.sort()
 
 
 for coord in coords:
-    row = [scorebookC[coord]]
+    row = [scorebookFN.get(coord, float(0)]
     print('\t'.join(map(str,row)))
 #for coord,score in scorebookD.items():
 #    row = [coord, score, scorebookFC[coord],scorebookC[coord],scorebookD[coord],countbookC[coord]]
